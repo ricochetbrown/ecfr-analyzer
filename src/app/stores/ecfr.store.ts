f70fe12d033c295d4accd7c5f4d8c4cabf62da80
ricochetbrown <ricochetbrown@gmail.com>
2025-02-12 19:18:50 -0500

    Updates

--- src/app/stores/ecfr.store.ts
+++ src/app/stores/ecfr.store.ts
@@ -159,7 +159,7 @@ export const EcfrStore = signalStore(
             const agencies = await firstValueFrom(_http.get('api/agencies'));
             patchState(state, { agencies: (agencies as any).agencies });
 
-            patchState(state, { selectedAgency: state.agencies()[0] });
+            patchState(state, { selectedAgency: state.agencies()[1] });
 
             const titles = await firstValueFrom(_http.get('api/titles'));
             patchState(state, { titles: (titles as any).titles });
